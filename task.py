961e3a88f7eafff6a136c9ff4b7bb6b5949ad724
Alicja <alicja@linux-ala>
2025-11-17 14:46:16 +0100

    Taks 2

--- task.py
+++ task.py
@@ -28,3 +28,30 @@
 #
 #Delete these comments before commit!
 #Good luck.
+
+names = ["Mikayla", "Nicholas", "Alanna", "Donovan", "Elisabeth", "Amari", "Melanie", "Solomon", "Sloan", "Alfredo", "Jaylah", "Gael", "Melanie", "Caspian"]
+surnames = ["Gray", "Shaffer", "Wolfe", "Jarvis", "Riley", "Sullivan", "Barber", "Mathews", "Stafford", "Beasley", "Fisher", "Sullivan", "Roach", "Heath", "Fuller", "Wilcox", "Russell", "McCarthy"]
+subject = ["math", "physics", "chemistry", "geography"]
+
+class Diary:
+    def __init__(self, name, surname, subject, attendance, grade):
+        self.name = name
+        self.surname = surname
+        self.subject = subject
+        self.attendance = attendance
+        self.grade = grade
+
+    def attendance_check(self, attendance):
+        if attendance == 1:
+            self.attendance += 1
+
+#diary:
+# For one subject
+# for various students
+# each students have some grades and attendance
+# each class a student get attendance, but not neccesarily a grade
+# each student should have their own diary, and teacher sees the attendance and average of grades
+
+if __name__ = "__main__":
+    Diary(names[0], surnames[0], subject, 1, 2)
+    Diary(names[1], surnames[1], subject, 2, 1)
